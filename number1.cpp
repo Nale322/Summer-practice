36d11f261880dffab8017866f628fc726698b2b0
Nale322 <nbo2013@mail.ru>
2019-06-09 12:41:16 +0300

    Исправил номера

--- number1.cpp
+++ number1.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-string bukovki(string g)
+int main()
 {
 	string s;
-	int l = 0, d = 0, f = 0, k = 0, h = 0, j = 0;
+	int l = 0, d = 0, f = 0, g = 0, h = 0, j = 0;
 	getline(cin,s);
 	s.length();
 	for (int p = 0; p < s.length(); p++)
@@ -19,20 +19,14 @@ string bukovki(string g)
 		if (s[p] == 'y')
 			f++;
 		if (s[p] == 'u')
-			k++;
+			g++;
 		if (s[p] == 'i')
 			h++;
 		if (s[p] == 'o')
 			j++;
 	}
-	cout << "a = " << l << " " << "e = " << d << " " << "y = " << f << " " << "u = " << k << " " << "i = " << h << " " << "o = " << j << endl;
-	return s;
-}
-
-int main()
-{
-	string s;
-    bukovki(s);
+	cout << "a = " << l << " " << "e = " << d << " " << "y = " << f << " " << "u = " << g << " " << "i = " << h << " " << "o = " << j << endl;
+	system("pause");
 	return 0;
 }
 
--- number2.cpp
+++ number2.cpp
@@ -4,19 +4,35 @@
 
 using namespace std;
 
-string bukovki(string s)
+string bukovki(string g)
 {
-    string g;
+	string s;
+	int l = 0, d = 0, f = 0, k = 0, h = 0, j = 0;
+	getline(cin,s);
+	s.length();
 	for (int p = 0; p < s.length(); p++)
 	{
-		if (s[p] == 'a'||s[p] == 'e'||s[p] == 'y'||s[p] == 'u'||s[p] == 'i'||s[p] == 'o')
-		g+=s[p];
-    }
-    return g;
+		if (s[p] == 'a')
+			l++;
+		if (s[p] == 'e')
+			d++;
+		if (s[p] == 'y')
+			f++;
+		if (s[p] == 'u')
+			k++;
+		if (s[p] == 'i')
+			h++;
+		if (s[p] == 'o')
+			j++;
+	}
+	cout << "a = " << l << " " << "e = " << d << " " << "y = " << f << " " << "u = " << k << " " << "i = " << h << " " << "o = " << j << endl;
+	return s;
 }
+
 int main()
 {
-    string s;
-    getline(cin, s);
-    cout << bukovki(s);
+	string s;
+    bukovki(s);
+	return 0;
 }
+
